2091f32799f35666ffd5d919c33a0d73c7f210e2
Hridoy Chandra Das <hridoycseboy@gmail.com>
2023-12-30 13:56:40 +0900

    first commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,108 @@
+plugins {
+    id(libs.plugins.android.application.get().pluginId)
+    id(libs.plugins.ksp.get().pluginId)
+    id(libs.plugins.kotlin.android.get().pluginId)
+    id(libs.plugins.kotlinter.get().pluginId)
+    alias(libs.plugins.paparazzi) apply false
+    alias(libs.plugins.hilt) apply false
+    id(libs.plugins.sortDependencies.get().pluginId)
+    id(libs.plugins.protobuf.get().pluginId)
+}
+
+android {
+    compileSdk = libs.versions.compileSdk.get().toInt()
+    namespace = "template"
+
+    defaultConfig {
+        applicationId = "template.app.id"
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+            buildConfigField("String", "Template_HOST", "\"not given\"")
+        }
+
+        debug {
+            buildConfigField("String", "Template_HOST", "\"192.168.10.34\"")
+        }
+    }
+
+    buildFeatures {
+        compose = true
+        buildConfig = true
+    }
+}
+
+dependencies {
+    ksp(libs.androidx.room.compiler)
+    // Hilt
+    ksp(libs.hilt.compiler)
+    ksp(libs.square.moshi.kotlin.codegen)
+
+    kspAndroidTest(libs.hilt.android.compiler)
+
+    // Gradle
+    implementation(platform(libs.compose.bom))
+    // UI
+    implementation(libs.accompanist.systemuicontroller)
+    implementation(libs.android.material)
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.appcompat)
+    // Navigation
+    implementation(libs.androidx.compose.navigation)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.hilt.compose.navigation)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    // Network and Local
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.compose.material3)
+    implementation(libs.compose.ui)
+    implementation(libs.compose.ui.tooling)
+    implementation(libs.datastore)
+    implementation(libs.hilt.android)
+    implementation(libs.ktor.client.core)
+    implementation(libs.protobuf.javalite)
+    implementation(libs.square.moshi.kotlin)
+    implementation(libs.square.retrofit)
+    implementation(libs.square.retrofit.converter.moshi)
+    implementation(libs.timber)
+
+    // Test
+    debugImplementation(platform(libs.compose.bom))
+    debugImplementation(libs.compose.ui.test.manifest)
+    debugImplementation(libs.compose.ui.tooling)
+    // Others
+    debugImplementation(libs.square.leakcanary)
+
+    annotationProcessor(libs.androidx.room.compiler)
+
+    testImplementation(libs.junit)
+
+    androidTestImplementation(platform(libs.compose.bom))
+    androidTestImplementation(libs.androidx.test.espresso.core)
+    androidTestImplementation(libs.androidx.test.junit)
+    androidTestImplementation(libs.compose.ui.test.junit)
+    androidTestImplementation(libs.hilt.android.testing)
+}
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:3.19.4"
+    }
+    generateProtoTasks {
+        all().forEach { task ->
+            task.plugins.create("java") {
+                option("lite")
+            }
+        }
+    }
+}
---
+++ build.gradle.kts
@@ -0,0 +1,180 @@
+import com.android.build.gradle.AppExtension
+import com.android.build.gradle.AppPlugin
+import com.android.build.gradle.BaseExtension
+import com.android.build.gradle.LibraryExtension
+import com.android.build.gradle.LibraryPlugin
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+@Suppress("DSL_SCOPE_VIOLATION")
+plugins {
+    id(libs.plugins.detektGradlePlugin.get().pluginId).version(libs.versions.detektGradlePlugin)
+    alias(libs.plugins.paparazzi) apply false
+    alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.ksp) apply false
+    id(libs.plugins.sortDependencies.get().pluginId).version(libs.versions.sortDependencies).apply(false)
+    alias(libs.plugins.kotlinter) apply false
+//    id("com.squareup.sort-dependencies").version(libs.versions.sortDependencies).apply(false)
+}
+
+buildscript {
+
+    repositories {
+        google()
+        mavenCentral()
+        maven(url = "https://plugins.gradle.org/m2/")
+        maven {
+            setUrl("https://jitpack.io")
+        }
+
+    }
+
+    dependencies {
+        classpath(libs.detekt.gradle.plugin)
+        classpath(libs.gradle)
+        classpath(libs.gradle.versions.plugin)
+        classpath(libs.kotlin.gradle.plugin)
+        classpath(libs.spotless)
+        classpath (libs.protobuf.gradle.plugin)
+
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle files
+    }
+
+    // Currently overriding the ktlint version to 1.0.1, can remove in the future when kotlinter
+    // is updated: https://github.com/jeremymailen/kotlinter-gradle#custom-ktlint-version
+    configurations.classpath {
+        resolutionStrategy {
+            force(
+                "com.pinterest.ktlint:ktlint-rule-engine:${libs.versions.ktlint.get()}",
+                "com.pinterest.ktlint:ktlint-rule-engine-core:${libs.versions.ktlint.get()}",
+                "com.pinterest.ktlint:ktlint-cli-reporter-core:${libs.versions.ktlint.get()}",
+                "com.pinterest.ktlint:ktlint-cli-reporter-checkstyle:${libs.versions.ktlint.get()}",
+                "com.pinterest.ktlint:ktlint-cli-reporter-json:${libs.versions.ktlint.get()}",
+                "com.pinterest.ktlint:ktlint-cli-reporter-html:${libs.versions.ktlint.get()}",
+                "com.pinterest.ktlint:ktlint-cli-reporter-plain:${libs.versions.ktlint.get()}",
+                "com.pinterest.ktlint:ktlint-cli-reporter-sarif:${libs.versions.ktlint.get()}",
+                "com.pinterest.ktlint:ktlint-ruleset-standard:${libs.versions.ktlint.get()}",
+            )
+        }
+    }
+}
+
+
+apply(from = "buildscripts/githooks.gradle")
+apply(from = "buildscripts/setup.gradle")
+apply(from = "buildscripts/versionsplugin.gradle")
+
+subprojects {
+    apply(from = "../buildscripts/detekt.gradle")
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.layout.buildDirectory)
+}
+
+afterEvaluate {
+    // We install the hook at the first occasion
+    tasks.named("clean") {
+        dependsOn(":installGitHooks")
+    }
+}
+
+tasks {
+    /**
+     * The detektAll tasks enables parallel usage for detekt so if this project
+     * expands to multi module support, detekt can continue to run quickly.
+     *
+     * https://proandroiddev.com/how-to-use-detekt-in-a-multi-module-android-project-6781937fbef2
+     */
+    @Suppress("UnusedPrivateMember")
+    val detektAll by registering(io.gitlab.arturbosch.detekt.Detekt::class) {
+        parallel = true
+        setSource(files(projectDir))
+        include("**/*.kt")
+        include("**/*.kts")
+        exclude("**/resources/**")
+        exclude("**/build/**")
+        config.setFrom(files("$rootDir/config/detekt/detekt.yml"))
+        buildUponDefaultConfig = false
+    }
+}
+
+fun BaseExtension.defaultConfig() {
+
+    compileSdkVersion(libs.versions.compileSdk.get().toInt())
+
+    defaultConfig {
+        minSdk = libs.versions.minSdk.get().toInt()
+        targetSdk = libs.versions.compileSdk.get().toInt()
+        consumerProguardFiles("consumer-rules.pro")
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    @Suppress("UnstableApiUsage")
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.composeCompiler.get()
+    }
+
+    packagingOptions {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+fun PluginContainer.applyDefaultConfig(project: Project) {
+    whenPluginAdded {
+        when (this) {
+            is AppPlugin -> {
+                project.extensions
+                    .getByType<AppExtension>()
+                    .apply {
+                        defaultConfig()
+                    }
+            }
+
+            is LibraryPlugin -> {
+                project.extensions
+                    .getByType<LibraryExtension>()
+                    .apply {
+                        defaultConfig()
+                    }
+            }
+
+            is JavaPlugin -> {
+                project.extensions.getByType<JavaPluginExtension>()
+                    .apply {
+                        sourceCompatibility = JavaVersion.VERSION_17
+                        targetCompatibility = JavaVersion.VERSION_17
+                    }
+            }
+        }
+    }
+}
+
+subprojects {
+    project.plugins.applyDefaultConfig(project)
+    afterEvaluate {
+        project.apply("${project.rootDir}/spotless.gradle")
+    }
+
+    tasks.withType<KotlinCompile> {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_17)
+            freeCompilerArgs.addAll(
+                listOf(
+                    "-opt-in=androidx.compose.material3.ExperimentalMaterial3Api"
+                )
+            )
+        }
+    }
+}
